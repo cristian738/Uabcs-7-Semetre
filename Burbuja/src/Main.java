f54a7fa85ba9c4bd31aa19454b0ffb64574cf02a
Cristian Manuel Rosas Gonzalez <Cristianrosasglez@gmail.com>
2016-01-19 15:55:47 -0700

    Burbuja
    metodo de la burbuja

---
+++ Burbuja/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] arreglo = {5,2,7,4};
+		
+		Metodo o = new Metodo();
+		o.Ordenar(arreglo);
+		
+		for(int i=0;i<arreglo.length;i++){
+			System.out.println(arreglo[i]);
+		}
+		
+	}
+
+}
---
+++ Burbuja/src/Metodo.java
@@ -0,0 +1,28 @@
+
+public class Metodo {
+	
+	public void Ordenar(int [] array){
+		int aux;
+		boolean cambios=false;
+		
+		while(true)
+		{	
+			cambios=false;
+			for(int i=1;i<array.length;i++)
+			{
+				if(array[i]<array[i-1]){
+					aux=array[i];
+					array[i]=array[i-1];
+					array[i-1]=aux;
+					cambios=true;
+				}
+			}
+			if(cambios==false)
+				break;
+		}
+	
+		
+	}
+	
+
+}
